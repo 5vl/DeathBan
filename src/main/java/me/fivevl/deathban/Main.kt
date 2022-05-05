0ecf3efec0a912838705008ff7eb4524eed75283
5vl <nederlandmax@gmail.com>
2022-05-05 20:03:29 +0200

    initial

---
+++ src/main/java/me/fivevl/deathban/Main.kt
@@ -0,0 +1,13 @@
+package me.fivevl.deathban
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
